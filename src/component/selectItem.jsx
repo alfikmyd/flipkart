73c558c15144096eab0cb664545d7aefe90f12f6
alfikmyd <alfikmyd@gmail.com>
2025-09-06 09:13:22 +0530

    coomit seven

--- src/component/selectItem.jsx
+++ src/component/selectItem.jsx
@@ -4,20 +4,134 @@ function SelectedItem() {
 
     return (
         <>
-        <div id="sepItem" style={{backgroundColor:"white",marginTop:"58px",marginLeft:"14.1%",marginRight:"14.1%"}}>
-            <div id="iDetail">
-                <div id="iLeft">
-                    <div className="listImage" style={{height:"768px"}}>
-                        <ul>
-                            <li><img src="https://rukminim2.flixcart.com/image/128/128/xif0q/computer/h/t/t/-original-imahcd9hhh6y4knb.jpeg?q=70&crop=false" alt="" /></li>
-                            <li><img src="" alt="" /></li>
-                        </ul>
+            <div id="sepItem" style={{ backgroundColor: "white", marginTop: "58px", marginLeft: "14.1%", marginRight: "14.1%" ,letterSpacing:".3px"}}>
+                <div id="iDetail">
+                    <div id="iLeft">
+                        <div className="iList">
+                            <div className="listImage" >
+                                <ul>
+                                    <li><img src="https://rukminim2.flixcart.com/image/128/128/xif0q/computer/h/t/t/-original-imahcd9hhh6y4knb.jpeg?q=70&crop=false" alt="" /></li>
+                                    <li><img src="https://rukminim2.flixcart.com/image/128/128/xif0q/computer/h/t/t/-original-imahcd9hhh6y4knb.jpeg?q=70&crop=false" alt="" /></li>
+                                    <li><img src="https://rukminim2.flixcart.com/image/128/128/xif0q/computer/r/4/p/-original-imahcd9hzmubfynx.jpeg?q=70&crop=false" alt="" /></li>
+                                    <li><img src="https://rukminim2.flixcart.com/image/128/128/xif0q/computer/n/t/s/-original-imahcd9hyrsyvp4p.jpeg?q=70&crop=false" alt="" /></li>
+                                    <li><img src="https://rukminim2.flixcart.com/image/128/128/xif0q/allinone-desktop/c/k/n/-original-imahcd99hb9jznvn.jpeg?q=70&crop=false" alt="" /></li>
+                                    <li><img src="https://rukminim2.flixcart.com/image/128/128/xif0q/allinone-desktop/x/z/z/-original-imahcd99kjrrunhu.jpeg?q=70&crop=false" alt="" /></li>
+                                    <li><img src="https://rukminim2.flixcart.com/image/128/128/xif0q/computer/j/f/d/-original-imahcd9hwvxytr4y.jpeg?q=70&crop=false" alt="" /></li>
+                                    <li><img src="https://rukminim2.flixcart.com/image/128/128/xif0q/computer/y/f/c/-original-imahcd9hb2qkwfcb.jpeg?q=70&crop=false" alt="" /></li>
+                                </ul>
+                            </div>
+                            <div className="iImage">
+                                <img src="https://rukminim2.flixcart.com/image/128/128/xif0q/computer/h/t/t/-original-imahcd9hhh6y4knb.jpeg?q=70&crop=false" alt="" />
+                                <div className="iWish">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="N1bADF" width="16" height="16" viewBox="0 0 20 16"><path d="M8.695 16.682C4.06 12.382 1 9.536 1 6.065 1 3.219 3.178 1 5.95 1c1.566 0 3.069.746 4.05 1.915C10.981 1.745 12.484 1 14.05 1 16.822 1 19 3.22 19 6.065c0 3.471-3.06 6.316-7.695 10.617L10 17.897l-1.305-1.215z" fill="#b8b5b5ff" className="x1UMqG" stroke="#FFF" fillRule="evenodd" opacity=".9"></path></svg>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="iButton">
+                            <button id="toCart"><svg className="NwyjNT" width="16" height="16" viewBox="0 0 16 15" xmlns="http://www.w3.org/2000/svg"><path class="" d="M15.32 2.405H4.887C3 2.405 2.46.805 2.46.805L2.257.21C2.208.085 2.083 0 1.946 0H.336C.1 0-.064.24.024.46l.644 1.945L3.11 9.767c.047.137.175.23.32.23h8.418l-.493 1.958H3.768l.002.003c-.017 0-.033-.003-.05-.003-1.06 0-1.92.86-1.92 1.92s.86 1.92 1.92 1.92c.99 0 1.805-.75 1.91-1.712l5.55.076c.12.922.91 1.636 1.867 1.636 1.04 0 1.885-.844 1.885-1.885 0-.866-.584-1.593-1.38-1.814l2.423-8.832c.12-.433-.206-.86-.655-.86" fill="#fff"></path></svg> ADD TO CART</button>
+                            <button id="buy"><svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="currentColor" className="bi bi-lightning-fill" viewBox="0 0 16 16">
+                                <path d="M5.52.359A.5.5 0 0 1 6 0h4a.5.5 0 0 1 .474.658L8.694 6H12.5a.5.5 0 0 1 .395.807l-7 9a.5.5 0 0 1-.873-.454L6.823 9.5H3.5a.5.5 0 0 1-.48-.641z" />
+                            </svg> BUY NOW</button>
+                        </div>
                     </div>
-                </div>
 
-                <div id="iRight"></div>
+                    <div id="iRight">
+                        <div className="iPath">
+                            <div style={{ color: "#949191ff" , fontSize:"11px"}}>
+                                <span>Home</span>
+                                <svg style={{ marginLeft: "6px" }} xmlns="http://www.w3.org/2000/svg" width="10" height="8" fill="currentColor" className="bi bi-chevron-right" viewBox="0 0 16 16">
+                                    <path fillRule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708" />
+                                </svg>
+                                <span style={{ marginLeft: "6px" }}>Computers</span>
+                                <svg style={{ marginLeft: "6px" }} xmlns="http://www.w3.org/2000/svg" width="10" height="8" fill="currentColor" className="bi bi-chevron-right" viewBox="0 0 16 16">
+                                    <path fillRule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708" />
+                                </svg>
+                                <span style={{ marginLeft: "6px" }}>Laptops</span>
+                                <svg style={{ marginLeft: "6px" }} xmlns="http://www.w3.org/2000/svg" width="10" height="8" fill="currentColor" className="bi bi-chevron-right" viewBox="0 0 16 16">
+                                    <path fillRule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708" />
+                                </svg>
+                                <span>Laptop Name...</span>
+                                <svg style={{ marginLeft: "6px" }} xmlns="http://www.w3.org/2000/svg" width="10" height="8" fill="currentColor" className="bi bi-chevron-right" viewBox="0 0 16 16">
+                                    <path fillRule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708" />
+                                </svg>
+                                <span>Laptop Intel...</span>
+
+                            </div>
+                            <div style={{ display: "flex", gap: "10px", marginLeft: "37%", marginTop: "-16px" }}>
+                                <label>
+                                    <input type="checkbox" value="compare" /> Compare
+                                </label>
+                                <p><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" className="jWKn+i"><path d="M14.78 5.883L9.032 0v3.362C3.284 4.202.822 8.404 0 12.606 2.053 9.666 4.927 8.32 9.032 8.32v3.446l5.748-5.883z" className="g9gS7K" fill="#c2c2c2" fillRule="evenodd"></path></svg>
+                                    <span> Share</span></p>
+                            </div>
+                        </div>
+
+                        <div className="ibrandDetail" style={{marginTop:"-25px"}}>
+                            <h3 style={{fontWeight:"400",fontSize:"17.5px"}}>CHUWI Intel Celeron Dual Core 11th Gen N4020 - (8 GB/256 GB SSD/Windows 11 Home) HeroBook Pro Laptop (14.1 inch, Grey, 1.39 kg)</h3>
+
+                            <div className="ibrandButton" style={{marginTop:"-18px"}}>
+                                <button style={{cursor:"pointer",height:"18px"}}>3.7 *</button>
+                                <span style={{ color: "#878787", fontWeight: "bold", fontSize: "13px",marginTop:"-5px" }}>1,728 Ratings & 207 Reviews</span>
+                            </div>
+                            <span style={{ color: "#388e3c", fontSize: "12px", fontWeight: "bold" ,marginTop:"-1px"}}>Specail price</span>
+
+                            <div className="iPrice">
+                                <span style={{ fontSize: "28px", fontWeight: "bold" }}>₹18,990</span>
+                                <span style={{ fontSize: "16px", color: "#878787", textDecoration: "line-through" }}>₹34,990</span>
+                                <span style={{ color: "#388e3c", fontSize: "16px", fontWeight: "bold" }}>45% off</span>
+                                <span className="i">i</span>
+                            </div>
+                            <span style={{marginTop:"-6px"}}>+ ₹59 Protect Promise Fee <a href="#">Learn more</a></span>
+                            <span style={{marginTop:"-10px"}}>Secure delivery by 11 Sep, Thursday</span>
+
+                            <div className="iOffers">
+                                <span style={{ fontSize: "16px", fontWeight: "bold" }}>Available offers</span>
+                                <span style={{marginTop:"-5px"}}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="20" fill="currentColor" className="bi bi-tag-fill" viewBox="0 0 16 16">
+                                        <path d="M2 1a1 1 0 0 0-1 1v4.586a1 1 0 0 0 .293.707l7 7a1 1 0 0 0 1.414 0l4.586-4.586a1 1 0 0 0 0-1.414l-7-7A1 1 0 0 0 6.586 1zm4 3.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0" />
+                                    </svg> <b>Bank Offer</b> 5% cashback on Flipkart Axis Bank Credit Card upto ₹4,000 per statement quarter <a href="#">T&C</a>
+                                </span>
+                                <span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-tag-fill" viewBox="0 0 16 16">
+                                        <path d="M2 1a1 1 0 0 0-1 1v4.586a1 1 0 0 0 .293.707l7 7a1 1 0 0 0 1.414 0l4.586-4.586a1 1 0 0 0 0-1.414l-7-7A1 1 0 0 0 6.586 1zm4 3.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0" />
+                                    </svg> <b>Bank Offer</b> 5% cashback on flipkart SBI Credit Card upto ₹4,000 per calendar quarter <a href="#">T&C</a>
+                                </span>
+                                <span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-tag-fill" viewBox="0 0 16 16">
+                                        <path d="M2 1a1 1 0 0 0-1 1v4.586a1 1 0 0 0 .293.707l7 7a1 1 0 0 0 1.414 0l4.586-4.586a1 1 0 0 0 0-1.414l-7-7A1 1 0 0 0 6.586 1zm4 3.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0" />
+                                    </svg> <b>Bank Offer</b> 5% cashback on Axis Bank Flipkart Debit Card <a href="#">T&C</a>
+                                </span>
+                                <span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-tag-fill" viewBox="0 0 16 16">
+                                        <path d="M2 1a1 1 0 0 0-1 1v4.586a1 1 0 0 0 .293.707l7 7a1 1 0 0 0 1.414 0l4.586-4.586a1 1 0 0 0 0-1.414l-7-7A1 1 0 0 0 6.586 1zm4 3.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0" />
+                                    </svg> <b>Special Price</b> Get extra 20% off (price inclusive of cashback/coupon) <a href="#">T&C</a>
+                                </span>
+                                <span><a href="#">View 2 more offers</a></span>
+                            </div> 
+
+                            <div className="selectExchange">
+                                <div id="excActive" style={{backgroundColor:"#f5faff"}}>
+                                    <label >
+                                        <input type="radio" checked/>
+                                        <span> Buy without Exchange</span>
+                                        <span style={{marginLeft:"40%",fontWeight:"bold"}}>₹18,990</span>
+                                    </label>
+                                </div>
+                                <div style={{display:"flex",flexDirection:"column",lineHeight:'20px',marginTop:"-1px",color:"#838282ff",cursor:"not-allowed"}}>
+                                    <label style={{marginTop:"5px",cursor:"not-allowed"}}>
+                                        <input type="radio" disabled style={{cursor:"not-allowed"}}/>
+                                        <span> Buy with Exchange</span>
+                                        <span style={{marginLeft:"30%"}}>up to ₹14,000 off</span>
+                                    </label>
+                                    <span style={{color:"red",marginTop:"10px",marginLeft:"25px"}}>Enter pincode to check if exchange is avilable</span>
+                                </div>
+                            </div>
+                        </div>
+
+
+                    </div>  
+                </div>
             </div>
-        </div>
         </>
     )
 }
